d811c8bcb66c4c54e960f9854257e367d39cb54a
Grace Lee <hyorim.lee.skorea@gmail.com>
2021-01-11 11:35:13 -0500

    first commit

---
+++ src/helper.py
@@ -0,0 +1,9 @@
+import os
+
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
